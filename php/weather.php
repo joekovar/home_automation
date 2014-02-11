8e614d4959a08723632dc598c7c5838d73c8af8d
joekovar <mr.joebert@gmail.com>
2014-02-10 23:35:40 -0500

    implement audio forecast

--- php/weather.php
+++ php/weather.php
@@ -4,12 +4,27 @@ class weather
 {
 	public static $forecast_url = 'http://api.wunderground.com/api/%1$s/forecast/q/%2$s.xml';
 	public static $forecast_cache = '/cache/weather/forecast.xml';
+	public static $forecast_xml = false;
 
 	public static $hourly_url = 'http://api.wunderground.com/api/%1$s/hourly/q/%2$s.xml';
 	public static $hourly_cache = '/cache/weather/hourly.xml';
-	
+	public static $hourly_xml = false;
+
 	public static $today_low = false;
 	public static $today_high = false;
+	
+	public static function say_forecast()
+	{
+		if(weather::get_forecast())
+		{
+			$fcttext = (string) weather::$forecast_xml->forecast->txt_forecast->forecastdays->forecastday->fcttext;
+			$fcttext = preg_replace('#(\d+)F#', '$1', $fcttext);
+
+			audio::tts($fcttext);
+			return true;
+		}
+		return false;
+	}
 
 	public static function today_low()
 	{
@@ -29,25 +44,42 @@ class weather
 		return weather::$today_high;
 	}
 	
-	protected static function highlow()
+	protected static function get_forecast($skip_cache = false)
 	{
+		if(weather::$forecast_xml !== false && $skip_cache === false)
+		{
+			return true;
+		}
 		global $config;
-
-		if( ! filemtime(ROOT_PATH . weather::$forecast_cache) || (time() - filemtime(ROOT_PATH . weather::$forecast_cache) >= 86400))
+		
+		if( ! @filemtime(ROOT_PATH . weather::$forecast_cache) || (time() - @filemtime(ROOT_PATH . weather::$forecast_cache) >= 86400))
 		{
 			if($xml = file_get_contents(sprintf(weather::$forecast_url, $config['wunderground-api-key'], $config['wunderground-api-location'])))
 			{
 				file_put_contents(ROOT_PATH . weather::$forecast_cache, $xml);
+			}
+			else
+			{
+				return false;
 			}
 		}
 		else
 		{
 			$xml = file_get_contents(ROOT_PATH . weather::$forecast_cache);
 		}
-
-		$xml = new SimpleXMLElement($xml);
 		
-		foreach($xml->forecast->simpleforecast->forecastdays->forecastday as $val)
+		weather::$forecast_xml = new SimpleXMLElement($xml);
+		return true;
+	}
+	
+	protected static function highlow()
+	{
+		if(weather::get_forecast() === false)
+		{
+			return false;
+		}
+		
+		foreach(weather::$forecast_xml->forecast->simpleforecast->forecastdays->forecastday as $val)
 		{
 			if($val->date->year == date('Y') && $val->date->month == date('n') && $val->date->day == date('j'))
 			{
