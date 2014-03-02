2bfb1c9f25c91ac676af0d8b659dfbec646d09b1
joekovar <mr.joebert@gmail.com>
2014-03-01 21:47:20 -0500

    added camera module

--- mobile.php
+++ mobile.php
@@ -73,5 +73,25 @@ p {font-size:0.75em;}
 	<li><a href="/mobile.php?pin=39">Car Door (<?php  echo ($pins[25]->state ? '<span class="on">Closed</span>' : '<span class="off">Open</span>'); ?>)</a></li>
 </ul>
 </div>
+<div class="section">
+<h3>Front Door Camera</h3>
+<p>
+<?php
+
+	shell_exec('rm ' . ROOT_PATH . '/cache/cameras/screenshot-1a.jpg');
+	
+	$screenshot	= "cache/cameras/screenshot-1a";
+	$_screenshot	= ROOT_PATH . "/{$screenshot}";
+
+	shell_exec(ROOT_PATH . "/backup/somagic-easycap_1.1/somagic-capture -i 1 -f 1 --vo={$_screenshot}.raw");
+	shell_exec("convert -size 720x480 'UYVY:{$_screenshot}.raw' -size 320x240 {$_screenshot}.jpg");
+
+	echo "<img src='{$screenshot}.jpg' style='width:320px; height:240px;'/>\n";
+
+
+?>
+</p>
+</div>
+
 </body>
 </html>
